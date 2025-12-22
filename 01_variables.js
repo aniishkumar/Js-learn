cc07396dc04e263a0cf34193d0ea23a9e11ad1e6
Anish Kumar <anishkumar112006@gmail.com>
2025-12-22 12:12:08 +0530

    learnt about variables

---
+++ 01_variables.js
@@ -0,0 +1,13 @@
+const accountId = 144553
+let accountEmail ="anish@gmail.com"
+var accountPassword = "12345"
+accountCity = "Hyderabad"
+
+accountEmail="asdfa@gmail.com"
+accountPassword="121221"
+accountCity="chennai"
+
+let accountState;
+//accountId =2// not allowed
+console.log(accountId);
+console.table([accountId,accountEmail,accountPassword,accountCity,accountState])
