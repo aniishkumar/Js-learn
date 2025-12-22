089c2b3f5464ab4da0c352a57a92c41fea928da0
Anish Kumar <anishkumar112006@gmail.com>
2025-12-22 17:49:01 +0530

    learnt about converstionOperations

---
+++ 03_converstionOperations.js
@@ -0,0 +1,6 @@
+// let booleanIsLoggiedIn = 1
+// let NumbooleanIsLoggedIn = Boolean(booleanIsLoggiedIn)
+// console.log(NumbooleanIsLoggedIn)
+let somenNumber=4324
+let numberInString=String(somenNumber)
+console.log(typeof numberInString)
