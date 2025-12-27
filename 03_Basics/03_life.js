a86eab5c4002812bf0b3fa01db923367b7e60609
Anish Kumar <anishkumar112006@gmail.com>
2025-12-27 15:40:45 +0530

    finished with how js works internally and functions

--- 03_Basics/03_life.js
+++ 03_Basics/03_life.js
@@ -1,5 +1,17 @@
-// immediately invoked functions 
+// immediately invoked function expressions 
 
-function chai(){
+(function chai(){
     console.log(`DB`)
-}
+})();
+
+(function anotherChai(){
+    console.log("hai")
+})();
+
+(()=>{
+    console.log("anish kumar")
+})();
+
+((name)=>{
+    console.log(`my name is ${name}`)
+})("anish")
