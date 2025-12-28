12079ac03663b73a2dc211aee634857602ae11aa
Anish Kumar <anishkumar112006@gmail.com>
2025-12-28 15:49:19 +0530

    finished up with part 1

---
+++ 04_control_flow/nine.js
@@ -0,0 +1,24 @@
+
+
+
+const shoppingCart = [
+    {
+        itemName: "js course",
+        price: 2999
+    },
+    {
+        itemName: "py course",
+        price: 999
+    },
+    {
+        itemName: "mobile dev course",
+        price: 5999
+    },
+    {
+        itemName: "data science course",
+        price: 12999
+    },
+]
+
+const totalPrice = shoppingCart.reduce((acc,currval)=>{return acc + currval.price},0)
+console.log(totalPrice)
